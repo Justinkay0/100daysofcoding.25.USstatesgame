aa34054413b68776924f3544ff5ce933c4b2cc95
Justin Kay <14783039+Justinkay0@users.noreply.github.com>
2023-03-28 00:22:18 +0800

    U.S. State Game 3 : saving states not answered to csv

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -21,6 +21,10 @@ max_country = len(states)
 answer_state = screen.textinput(title='Guess the state', prompt="What's another state's name")
 while num_country < max_country:
     answer_state = answer_state.title()
+
+    if answer_state == 'Exit':
+        break
+
     if answer_state in states and answer_state not in country_done:
         cord = get_xy(answer_state, df)
         freedom = turtle.Turtle()
@@ -33,5 +37,10 @@ while num_country < max_country:
     answer_state = screen.textinput(title=f'{num_country}/{max_country} States correct',\
                                     prompt="What's another state's name")
 
-# turtle.onscreenclick(get_mouse_click_coor)
-turtle.mainloop()
+
+learn_states = []
+for i in states:
+    if i not in country_done:
+        learn_states.append(i)
+learn_states = pd.DataFrame(learn_states,columns=['states'])
+learn_states.to_csv('states_to_learn.csv')
