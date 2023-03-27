200a153e78f055a72da93f1f2a8d36bdc278bab3
Justinkay0 <14783039+Justinkay0@users.noreply.github.com>
2023-03-27 17:58:18 +0800

    Creating a count of fur colors from central park census data with pandas

--- main.py
+++ main.py
@@ -9,16 +9,30 @@
 #
 #
 # print(temperature)
+#
+# import pandas as pd
+#
+# data = pd.read_csv('weather_data.csv')
+#
+# temp_list = data['temp'].to_list()
+# # print(data['temp'].mean())
+# # print(data['temp'].max())
+# # print(data[data.temp == data.temp.max()])
+#
+# monday = data[data.day == 'Monday']
+# # printing fahrenheit from Celsius
+# print((monday.temp * 1.8) + 32)
 
 import pandas as pd
 
-data = pd.read_csv('weather_data.csv')
+df = pd.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+squirrel_count = df['Primary Fur Color'].groupby(df['Primary Fur Color']).count().to_dict()
 
-temp_list = data['temp'].to_list()
-# print(data['temp'].mean())
-# print(data['temp'].max())
-# print(data[data.temp == data.temp.max()])
+val1 = []
+val2 = []
+for key, value in squirrel_count.items():
+    val1.append(key)
+    val2.append(value)
 
-monday = data[data.day == 'Monday']
-# printing fahrenheit from Celsius
-print((monday.temp * 1.8) + 32)
+data = pd.DataFrame({'Fur Color': val1, 'Count':val2})
+data.to_csv('squirrel_count.csv')
