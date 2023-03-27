8ce5d1a68531d50e4dcb72fae78c73fa438e4b16
Justinkay0 <14783039+Justinkay0@users.noreply.github.com>
2023-03-27 17:13:12 +0800

    inserting temperature

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import csv
 
 with open('weather_data.csv') as data_file:
     data = csv.reader(data_file)
+    temperature = []
     for row in data:
-        print(row)
+        if row[1] != 'temp':
+            temperature.append(int(row[1]))
+
+
+print(temperature)
